687bdcf0709ab1cac65d90cc3e112a34c809c622
saya <geraldiakmal612@gmail.com>
2024-09-11 10:00:17 +0700

    penambahan file

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # DASPRO JOBSHEET 3
+Program pembuatan peng absenan dan penyetoran nilai khusus mahasiswa pollinema
--- Siakad11_modifikasi.java
+++ Siakad11_modifikasi.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Siakad11{
+public class Siakad11_modifikasi{
     public static void main(String[] args){
 
         //konfigurasi scanner
@@ -10,7 +10,7 @@ public class Siakad11{
         String nama,nim;//string data type untuk storing text
         char kelas;//char untuk storing satu karakter huruf/ASCII
         byte absen;//byte menyimpan angka dari -128 sd 127
-        Double nilai_kuis, nilai_tugas, nilai_ujian,nilai_akhir;//double untuk menyimpan bilangan pecahan,dpt menyimpang hingga 1.7976931348623157E+308
+        Double nilai_kuis, nilai_tugas, nilai_uts,nilai_uas,nilai_akhir;//double untuk menyimpan bilangan pecahan,dpt menyimpang hingga 1.7976931348623157E+308
 
         System.out.print("masukan nama : ");
         nama = inputan.nextLine();//nextLine utk ngambil string input
@@ -24,16 +24,22 @@ public class Siakad11{
         System.out.print("Masukan Nomor Absen : ");
         absen = inputan.nextByte() ;//nextByte utk mengambil Byte input
 
-        System.out.print("Masukan Nilai Quis : ");
-        nilai_kuis = inputan.nextDouble();//nextDouble utk input dalam bentuk double
+        System.out.print("Masukan Nilai uts : ");
+        nilai_uts = inputan.nextDouble();//nextDouble utk input dalam bentuk double
 
         System.out.print("masukan nilai tugas : ");
         nilai_tugas = inputan.nextDouble();
 
-        System.out.print("masukan nilai ujian : ");
-        nilai_ujian = inputan.nextDouble();
+        System.out.print("masukan nilai uas : ");
+        nilai_uas = inputan.nextDouble();
 
-        nilai_akhir = (nilai_kuis + nilai_tugas + nilai_ujian)/3;
+        System.out.print("masukan nilai kuis : ");
+        nilai_kuis = inputan.nextDouble();
+
+        nilai_akhir=(nilai_kuis*20/100 + nilai_tugas*15/100 + nilai_uts*30/100 + nilai_uas*35/100);
+
+
+        
 
         System.out.print("Mahasiswa dengan nama "+nama +" "+"(NIM : "+nim+")");
         System.out.println(" "+"Kelas : " +kelas +" "+"Nomor Absen"+" " +absen);
